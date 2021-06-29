50c06ce701355a81866dd9bdd21a1fbb7c4056ce
Adam L <alane019@gmail.com>
2021-06-28 23:17:46 -0400

    update class to className

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
 	return (
 		<BrowserRouter>
-			<div id="app-outer" class="text-center">
+			<div id="app-outer" className="text-center">
 
 				<Switch>
 						<Route path="/quiz" component={Quiz} exact />
